5ef59eeccdf078bfdd0f2fafdd69e3c97b0f58e4
yunusj <contact@creativityblast.com>
2017-11-23 04:27:32 +0500

    navigation changed to ant design

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -18,11 +18,14 @@ const navItems = [
                     }
                 ]
 
+let current = 0;
+
 export default class Header extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            navItems: this.getNavItems(this.props)
+            navItems: this.getNavItems(this.props),
+            currentPage: current
         }
     }
 
@@ -30,13 +33,15 @@ export default class Header extends Component {
         if(this.props.currentPage != nextProps.currentPage)
         {
             this.setState({
-                navItems: this.getNavItems(nextProps)
+                navItems: this.getNavItems(nextProps),
+                currentPage: current
             });
         }
     }
 
     getNavItems(props) {
         return navItems.map((listValue, index)=>{
+            props.currentPage.toLowerCase()==listValue.name.toLowerCase() ? current = index : null;
             return {
                 ...listValue,
                 current: (props.currentPage.toLowerCase()==listValue.name.toLowerCase())?true:false
@@ -48,7 +53,7 @@ export default class Header extends Component {
         return (
             <header className="header">
  				<Logo color="white" />
-                <Navigation togglePage={this.props.togglePage} items={this.state.navItems} />
+                <Navigation currentPage={this.state.currentPage} togglePage={this.props.togglePage} items={this.state.navItems} />
             </header>
         )
     }
--- src/Components/reusableComponents/Navigation.js
+++ src/Components/reusableComponents/Navigation.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
+import { Button, Menu, Icon } from 'antd';
+const MenuItemGroup = Menu.ItemGroup;
 
 export default class Navigation extends Component {
     render() {
         return (
-            <nav>
-                <ul>
-                    {this.props.items.map((listValue, index)=>{
-                        return <li key={index} data-item={listValue.name} className={(listValue.current)?'active':''} onClick={this.props.togglePage}>{listValue.name}</li>;
-                    })}
-                </ul>
+            <nav className="NAVIGATION">
+               <Menu
+               selectedKeys={[this.props.currentPage.toString()]}
+               mode="horizontal"
+               >
+                  {this.props.items.map((listValue, index)=>{
+                     return (<Menu.Item key={index} >
+                        <span onClick={this.props.togglePage} data-item={listValue.name}>{listValue.name}</span>
+                     </Menu.Item>);
+                  })}
+                  <Menu.Item key={`login`} >
+                     <Button type={`primary`}>Login</Button>
+                  </Menu.Item>
+               </Menu>
             </nav>
         )
     }
